3a85ff4a0260f1e3277b9c72179843c240fd3e24
Aarni Koskela <akx@iki.fi>
2023-03-27 11:19:10 +0300

    Match CSVSerializer.serialize()'s type to the parent class

--- connectors/serializers/csv.py
+++ connectors/serializers/csv.py
@@ -1,12 +1,14 @@
 import csv
+from collections.abc import Iterable
+from typing import Any
 
 from connectors.serializers.base import BaseSerializer
 
 
 class CSVSerializer(BaseSerializer):
-    def serialize(self, rows: tuple[list[str]], columns: list[str]):
+    def serialize(self, data: Iterable[Iterable[Any]], columns: list[str]):
         with open(self.output_path, "w") as f:
             writer = csv.writer(f)
             writer.writerow(columns)
-            for row in rows:
+            for row in data:
                 writer.writerow([str(val) for val in row])
