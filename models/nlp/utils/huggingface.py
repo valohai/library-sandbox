7d0910f4201737a357c632a71683da792869871a
Aarni Koskela <akx@iki.fi>
2023-05-22 17:18:09 +0300

    Allow load_huggingface_model_and_tokenizer to load from Huggingface Hub too

--- models/nlp/utils/huggingface.py
+++ models/nlp/utils/huggingface.py
@@ -1,37 +1,48 @@
+from __future__ import annotations
+
 import tempfile
 import zipfile
+from typing import TYPE_CHECKING
 
 import valohai
 from transformers import (
-    AutoModelForQuestionAnswering,
-    AutoModelForSeq2SeqLM,
-    AutoModelForSequenceClassification,
     AutoTokenizer,
     PreTrainedModel,
     PreTrainedTokenizer,
     TrainerCallback,
 )
 
+if TYPE_CHECKING:
+    from transformers.models.auto.auto_factory import _BaseAutoModelClass
+
 
 def load_huggingface_model_and_tokenizer(
-    archive_path: str,
-    model_type: type[
-        AutoModelForQuestionAnswering
-        | AutoModelForSeq2SeqLM
-        | AutoModelForSequenceClassification
-    ],
+    model_spec: str,
+    model_type: type[_BaseAutoModelClass],
     tokenizer_type: type[PreTrainedTokenizer] = AutoTokenizer,
 ) -> tuple[PreTrainedTokenizer, PreTrainedModel]:
-    if not archive_path.endswith(".zip"):
-        raise ValueError(f"Model {archive_path} is not a zip file. Aborting.")
+    """
+    Load a HuggingFace model and tokenizer from a model spec (pathname to a .zip or a model identifier on Hub).
+    """
+    if model_spec.endswith(".zip"):
+        return _load_from_zip(model_spec, model_type=model_type, tokenizer_type=tokenizer_type)
+    tokenizer = tokenizer_type.from_pretrained(model_spec)
+    model = model_type.from_pretrained(model_spec)
+    return tokenizer, model
 
+
+def _load_from_zip(
+    model_spec: str,
+    *,
+    model_type: type[_BaseAutoModelClass],
+    tokenizer_type: type[PreTrainedTokenizer],
+):
     with tempfile.TemporaryDirectory() as temp_dir:
-        with zipfile.ZipFile(archive_path, "r") as zip_ref:
+        with zipfile.ZipFile(model_spec, "r") as zip_ref:
             zip_ref.extractall(temp_dir)
 
         tokenizer = tokenizer_type.from_pretrained(temp_dir)
         model = model_type.from_pretrained(temp_dir)
-
     return tokenizer, model
 
 
